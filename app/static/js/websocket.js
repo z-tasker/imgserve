1618499eccce9b44f229439c3bea648a4a231028
mgrasker <mgraskertheband@gmail.com>
2020-08-01 18:37:28 -0400

    add tap event listener for surfaces

--- app/static/js/websocket.js
+++ app/static/js/websocket.js
@@ -21,6 +21,7 @@ function getImageFromFormWebsocket(action, values_from, img_target, use_id) {
 
     values_from.forEach(addToRequest);
 
+ //   var websocket_address = "ws://localhost:8080/data";
     var websocket_address = "wss://compsyn.fourtheye.xyz/data";
     var websocket = new WebSocket(websocket_address)
 
@@ -30,7 +31,7 @@ function getImageFromFormWebsocket(action, values_from, img_target, use_id) {
     };
     websocket.onmessage = function(mesg) {
         var data = JSON.parse(mesg.data);
-        console.log(`${websocket_address} responded with ${data.status}`)
+        console.log(`${websocket_address} responded with ${JSON.stringify(data)}`)
         var img = getTaggedElement(img_target, use_id);
         switch (data.status) {
             case 200:
@@ -44,4 +45,21 @@ function getImageFromFormWebsocket(action, values_from, img_target, use_id) {
     websocket.onclose = function() {
         console.log("websocket closing.");
     };
+}
+
+function bindSubmitButton(submitButton) {
+    submitButton.addEventListener("click tap", 
+        getImageFromFormWebsocket(
+            action="get", 
+            values_from=["get", "experiment"], 
+            img_target="colorgram",
+            use_id=submitButton.id.replace("submit-", "")
+        )
+    )
+}
+
+var buttonElements = document.getElementsByClassName("ws-submit");
+var i;
+for (i = 0; i < buttonElements.length; i++) {
+  bindSubmitButton(buttonElements[i].style.backgroundColor);
 }
---
+++ app/up.sh
@@ -0,0 +1,9 @@
+poetry run ./server.py \
+  --elasticsearch-client-fqdn "${ES_CLIENT_FQDN}" \
+  --elasticsearch-username "${ES_USERNAME}" \
+  --elasticsearch-password "${ES_PASSWORD}" \
+  --s3-bucket "${AWS_BUCKET_NAME}" \
+  --s3-region-name "${AWS_REGION_NAME}" \
+  --s3-access-key-id "${AWS_ACCESS_KEY_ID}" \
+  --s3-secret-access-key "${AWS_SECRET_ACCESS_KEY}" \
+  --debug
